88c045649c99401f2caec51d22a1da7bcb6c4154
滑川卓弥 <tatatatatata89@gmail.com>
2023-01-03 18:52:42 +0900

    例外処理

---
+++ src/main/java/sample_2.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class sample_2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int ticket = 100;
+        int b = sc.nextInt();
+
+
+        System.out.println(ticket + b * 10);
+    }
+}
---
+++ src/main/java/sample_3.java
@@ -0,0 +1,17 @@
+public class sample_3 {
+    public static void main(String[] args) {
+        String s = "+Paiza+";
+
+        for (int i = 0; i < s.length(); i++) {
+            System.out.print("+");
+
+        }
+        System.out.println("");
+        System.out.println(s);
+
+        for (int i = 0; i < s.length(); i++) {
+            System.out.print("+");
+
+        }
+    }
+}
